937cf8242c261c0a6d4af25e04823f81385a46b3
sekarasiewicz <sekarasiewicz@gmail.com>
2026-09-01 13:03:35 +0200

    Initial Commit

---
+++ Speaky/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Speaky
+//
+//  Created by Sebastian Karasiewicz on 01/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Speaky/SpeakyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakyApp.swift
+//  Speaky
+//
+//  Created by Sebastian Karasiewicz on 01/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpeakyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
